52ee8a432e14e21ee900f1425a65bae5c1de1662
«Zhasaral <zhasaralture@gmail.com>
2017-11-23 22:20:37 +0600

    50%

--- js/app.js
+++ js/app.js
@@ -3,93 +3,90 @@ app.config(function($routeProvider){
 
     $routeProvider.when("/addBook", {
         templateUrl:'templates/addBook.html',
-        controller:'menuOneController'
+        controller:'AppCtrl'
 
     })
 
     $routeProvider.when("/bookList", {
-        templateUrl:'templates/bookLlist.html',
-        controller:'menuTwoController'
+        templateUrl:'templates/bookList.html',
+        controller:'AppCtrl'
 
     })
     $routeProvider.when("/edit", {
-        templateUrl:'templates/bookLlist.html',
-        controller:'menuThreeController'
+        templateUrl:'templates/edit.html',
+        controller:'AppCtrl'
 
     })
     $routeProvider.when("/bookDelete", {
         templateUrl:'templates/bookDelete.html',
-        controller:'bookDeleteController'
+        controller:'AppCtrl'
 
     })
 
     $routeProvider.otherwise({redirectTo: '/'});
 })
-var books = [];
+ var books = [];
+
 
 
 Book.idCounter = 0;
-function Book(titleValue) {
+function Book(titleValue,priceValue,imageValue) {
     var id = ++Book.idCounter;
     var title = titleValue;
-    var image = image;
-    var avtors = [];
-    var genres = [];
-    this.getId              = function()      {
+    var price = priceValue;
+    var image = imageValue;
+
+
+    this.getId = function()     {
         return id;
     }
-    this.getTitle              = function()      {
+    this.getTitle = function()  {
         return title;
+    }
+    this.getPrice = function () {
+        return price;
+    }
+    this.getImage = function () {
+        return image;
+    }
+    this.setImage = function(value) {
+        image = value; 
+    }
+    this.setTitle = function (value) {
+        title = value;
+    }
+    this.setPrice = function (value) {
+        price = value;
     }
 }
 
-app.controller('menuOneController', function($scope,localStorageService){
+app.controller('AppCtrl',['$scope','$routeParams', function ($scope,$routeParams) {
+    $scope.books = [];
+    $scope.$on('$routeChangeSuccess', function()
+    {
+        $scope.films = books;
+
+        for (var i = 0; i < books.length; i++)
+        {
+            var book = books[i];
+            console.log("**************************");
+            console.log(book.getId());
+            console.log(book.getTitle());
+
+        }
+
+    });
+
     $scope.addNewBook=function () {
 
-        books.push(new Book($scope.bookTitle));
-        var options = "";
-        for (var i = 0; i < books.length; i++)
-            options += "<option value = " + books[i].getTitle() + " />";
+          var book = new Book($scope.bookTitle, $scope.price,'img/' + $('#poster').val().split('\\').pop());
+         var books = $scope.books;
+        books.push(book);
+        console.log(books[0].getTitle());
+        console.log(books[0].getId());
 
-        localStorageService.set('key',options);
-        console.log(books[1]);
-        console.log(books[2]);
-    }
+    };
 
 
-})
-app.controller('menuTwoController', function($scope,localStorageService){
-  $scope.books  = localStorageService.get('key');
-
-
-})
-app.controller('menuThreeController', function($scope,localStorageService){
-   var  blist = '';
-
-    var bookIndex;
-    for (var i = 0; i < books.length; i++)
-        bookIndex = i;
-   blist = books[bookIndex].getTitle();
-    $scope.booktitle =blist;
-
-
-})
-app.controller('bookDeleteController', function($scope,localStorageService){
-    $scope.books  = localStorageService.get('key');
-
-
-})
-
-
-
-app.controller('AppCtrl', function ($scope,localStorageService) {
-
-
-
-
-
-
-
-
-});
+}]);
 
